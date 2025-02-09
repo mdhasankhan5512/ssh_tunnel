66df4c652e5cecd5833a9bd912ce5e898a984b95
mdhasankhan5512 <131230205+mdhasankhan5512@users.noreply.github.com>
2025-02-09 19:58:31 +0600

    Update setup-ssh-tunnel.sh

--- setup-ssh-tunnel.sh
+++ setup-ssh-tunnel.sh
@@ -12,4 +12,13 @@ wget https://raw.githubusercontent.com/mdhasankhan5512/ssh_tunnel/refs/heads/mai
 mv custom_ssh_tunnel.lua /usr/lib/lua/luci/model/cbi/
 wget https://raw.githubusercontent.com/mdhasankhan5512/ssh_tunnel/refs/heads/main/controllar.lua
 mv controllar.lua /usr/lib/lua/luci/controller/custom_ssh_tunnel.lua
+cat <<EOF > /etc/config/custom_ssh_tunnel
+config tunnel 'settings'
+    option host '206.189.80.210'
+    option port '443'
+    option username 'racevpn.com-alyan26'
+    option password 'H5512552'
+    option sni 'cdn.snapchat.com'
+    option local_port '8080'
+EOF
 /etc/init.d/rpcd restart

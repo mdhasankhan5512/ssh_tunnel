93d21a27fd926186270f19500fb763d39a3c4482
mdhasankhan5512 <131230205+mdhasankhan5512@users.noreply.github.com>
2025-02-19 10:57:24 +0600

    Update setup-ssh-tunnel.sh

--- setup-ssh-tunnel.sh
+++ setup-ssh-tunnel.sh
@@ -14,11 +14,15 @@ wget https://raw.githubusercontent.com/mdhasankhan5512/ssh_tunnel/refs/heads/mai
 mv controllar.lua /usr/lib/lua/luci/controller/custom_ssh_tunnel.lua
 cat <<EOF > /etc/config/custom_ssh_tunnel
 config tunnel 'settings'
-    option host '206.189.80.210'
-    option port '443'
-    option username 'racevpn.com-alyan26'
-    option password 'H5512552'
+    option current_server '139.59.235.231:443@racevpn.com-alyan36:H5512552:24-02-2025'
     option sni 'cdn.snapchat.com'
     option local_port '8080'
+    list servers '139.59.235.231:443@racevpn.com-alyan36:H5512552:24-02-2025'
+    list servers 'sg4.tun1.pro:443@sshstores-shayan34:H5512552:26-02-2025'
 EOF
+wget https://raw.githubusercontent.com/mdhasankhan5512/ssh_tunnel/refs/heads/main/zzzz
+mv zzzz /etc/init.d/
+chmod +x /etc/init.d/zzzz
+service zzzz enable
+wget https://raw.githubusercontent.com/mdhasankhan5512/ssh_tunnel/refs/heads/main/check.py
 /etc/init.d/rpcd restart

3490b48f9ca4f569fd774173bd2b316a19acbf2d
mdhasankhan5512 <131230205+mdhasankhan5512@users.noreply.github.com>
2025-06-16 23:07:44 +0600

    Update setup-ssh-tunnel.sh

--- setup-ssh-tunnel.sh
+++ setup-ssh-tunnel.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 opkg update
 opkg install tmux sshpass autossh openssh-client openssl-util python3
-wget https://raw.githubusercontent.com/mdhasankhan5512/ssh_tunnel/refs/heads/main/zzz
+wget https://raw.githubusercontent.com/mdhasankhan5512/ssh-tunnel/refs/heads/main/zzz
+rm /etc/init.d/zzz
 mv zzz /etc/init.d/
 chmod +x /etc/init.d/zzz
 service zzz enable
@@ -10,11 +11,11 @@ mv tunnel.sh /root/
 chmod +x /root/tunnel.sh
 wget https://raw.githubusercontent.com/mdhasankhan5512/ssh-tunnel/refs/heads/main/custom_ssh_tunnel.lua
 mv custom_ssh_tunnel.lua /usr/lib/lua/luci/model/cbi/
-wget https://raw.githubusercontent.com/mdhasankhan5512/ssh_tunnel/refs/heads/main/controllar.lua
-mv controllar.lua /usr/lib/lua/luci/controller/custom_ssh_tunnel.lua
+wget https://raw.githubusercontent.com/mdhasankhan5512/ssh-tunnel/refs/heads/main/controllar.lua
+mv controllar.lua /usr/lib/lua/luci/controller/custom_ssh-tunnel.lua
 wget https://raw.githubusercontent.com/mdhasankhan5512/ssh-tunnel/refs/heads/main/conf
 mv conf /etc/config/custom_ssh_tunnel
-wget https://raw.githubusercontent.com/mdhasankhan5512/ssh_tunnel/refs/heads/main/zzzz
+wget https://raw.githubusercontent.com/mdhasankhan5512/ssh-tunnel/refs/heads/main/zzzz
 mv zzzz /etc/init.d/
 chmod +x /etc/init.d/zzzz
 service zzzz enable

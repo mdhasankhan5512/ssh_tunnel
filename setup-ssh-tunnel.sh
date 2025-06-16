a2ae506c7a80a77e378ef2e8afc730ec3163040f
mdhasankhan5512 <131230205+mdhasankhan5512@users.noreply.github.com>
2025-06-16 23:12:57 +0600

    Update setup-ssh-tunnel.sh

--- setup-ssh-tunnel.sh
+++ setup-ssh-tunnel.sh
@@ -7,17 +7,23 @@ mv zzz /etc/init.d/
 chmod +x /etc/init.d/zzz
 service zzz enable
 wget https://raw.githubusercontent.com/mdhasankhan5512/ssh-tunnel/refs/heads/main/tunnel.sh
+rm /root/tunnel.sh
 mv tunnel.sh /root/
 chmod +x /root/tunnel.sh
 wget https://raw.githubusercontent.com/mdhasankhan5512/ssh-tunnel/refs/heads/main/custom_ssh_tunnel.lua
+rm /usr/lib/lua/luci/model/cbi/custom_ssh_tunnel.lua
 mv custom_ssh_tunnel.lua /usr/lib/lua/luci/model/cbi/
 wget https://raw.githubusercontent.com/mdhasankhan5512/ssh-tunnel/refs/heads/main/controllar.lua
+rm /usr/lib/lua/luci/controller/custom_ssh-tunnel.lua
 mv controllar.lua /usr/lib/lua/luci/controller/custom_ssh-tunnel.lua
 wget https://raw.githubusercontent.com/mdhasankhan5512/ssh-tunnel/refs/heads/main/conf
+rm /etc/config/custom_ssh_tunnel
 mv conf /etc/config/custom_ssh_tunnel
 wget https://raw.githubusercontent.com/mdhasankhan5512/ssh-tunnel/refs/heads/main/zzzz
+rm /etc/init.d/zzzz
 mv zzzz /etc/init.d/
 chmod +x /etc/init.d/zzzz
 service zzzz enable
+rm check.py
 wget https://raw.githubusercontent.com/mdhasankhan5512/ssh-tunnel/refs/heads/main/check.py
 /etc/init.d/rpcd restart
